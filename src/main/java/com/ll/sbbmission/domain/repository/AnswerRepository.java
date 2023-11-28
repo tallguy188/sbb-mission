8e4cab3249527e65e8c9ec4797b732798d52a438
tallguy188 <cksghd4220@naver.com>
2023-11-29 06:50:56 +0900

    testing JPA

---
+++ src/main/java/com/ll/sbbmission/domain/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sbbmission.domain.repository;
+
+import com.ll.sbbmission.domain.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer,Integer> {
+}
--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -1,13 +1,44 @@
 package com.ll.sbbmission;
 
+import com.ll.sbbmission.domain.entity.Answer;
+import com.ll.sbbmission.domain.entity.Question;
+import com.ll.sbbmission.domain.repository.AnswerRepository;
+import com.ll.sbbmission.domain.repository.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.swing.text.html.Option;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbMissionApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private QuestionRepository questionRepository;
 
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+
+    @Transactional
+    @Test
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(8);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+    }
 }
