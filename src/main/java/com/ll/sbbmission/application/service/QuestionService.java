42ebbc9918a6fac161488dd2d003485390d700ee
tallguy188 <cksghd4220@naver.com>
2023-11-29 11:15:09 +0900

    make Question Detail

--- src/main/java/com/ll/sbbmission/application/service/QuestionService.java
+++ src/main/java/com/ll/sbbmission/application/service/QuestionService.java
@@ -1,12 +1,14 @@
 package com.ll.sbbmission.application.service;
 
 
+import com.ll.sbbmission.DataNotFoundException;
 import com.ll.sbbmission.domain.entity.Question;
 import com.ll.sbbmission.domain.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,18 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        }
+        else {
+            throw new DataNotFoundException("question not found");
+        }
+
     }
 
 
--- src/main/java/com/ll/sbbmission/presentation/controller/QuestionController.java
+++ src/main/java/com/ll/sbbmission/presentation/controller/QuestionController.java
@@ -8,23 +8,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 
 @RequiredArgsConstructor
+@RequestMapping("/question")
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     //@ResponseBody
     public String list(Model model) {
         List<Question>questionList = this.questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question",question);
+        return "question_detail";
     }
 
 }
